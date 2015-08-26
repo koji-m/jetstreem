8d4da4170f0eccf946629cd915b53f3df2919999
Koji Matsumoto <otomustam.ijok@gmail.com>
2015-08-26 20:58:47 +0900

    add class StrmQueue

---
+++ pw/koj/jetstreem/core/StrmQueue.java
@@ -0,0 +1,19 @@
+package pw.koj.jetstreem.core;
+
+import java.util.concurrent.LinkedBlockingQueue;
+
+public final class StrmQueue extends LinkedBlockingQueue {
+    private static StrmQueue instance;
+
+    private StrmQueue() {
+        super();
+    }
+
+    public static synchronized StrmQueue getInstance() {
+        if (instance == null) {
+            instance = new StrmQueue();
+        }
+        return instance;
+    }
+}
+
