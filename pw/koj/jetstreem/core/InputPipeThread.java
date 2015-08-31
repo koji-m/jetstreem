68dd613315cbc19f649a1ce9e32809f83f8d982b
Koji Matsumoto <otomustam.ijok@gmail.com>
2015-08-31 21:25:07 +0900

    add run() to class InpuPipeThread

--- pw/koj/jetstreem/core/InputPipeThread.java
+++ pw/koj/jetstreem/core/InputPipeThread.java
@@ -1,19 +1,36 @@
 package pw.koj.jetstreem.core;
 
 import java.io.*;
-import java.nio.channels.SelectableChannel;
+import java.nio.ByteBuffer;
+import java.nio.channels.Pipe.SinkChannel;
 
 public class InputPipeThread extends Thread {
     private InputStream in;
-    private SelectableChannel ch;
+    private SinkChannel ch;
 
-    public InputPipeThread(InputStream in, SelectableChannel ch) {
+    public InputPipeThread(InputStream in, SinkChannel ch) {
         this.in = in;
         this.ch = ch;
     }
 
     public void run() {
-
+        try {
+            try {
+                byte[] buf = new byte[4096];
+                ByteBuffer bbuf = ByteBuffer.wrap(buf);
+                int len;
+                while ((len = this.in.read(buf)) > 0) {
+                    bbuf.rewind();
+                    bbuf.limit(len);
+                    this.ch.write(bbuf);
+                }
+            } finally {
+                this.in.close();
+                this.ch.close();
+            }
+        } catch (IOException e) {
+            System.err.println("IOException: " + e.getMessage());
+        }
     }
 }
 
