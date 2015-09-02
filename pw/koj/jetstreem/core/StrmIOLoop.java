25655dea63cdd532afb7837288eecf756488223a
Koji Matsumoto <otomustam.ijok@gmail.com>
2015-09-02 22:05:32 +0900

    add misc methods in class StrmIOLoop

--- pw/koj/jetstreem/core/StrmIOLoop.java
+++ pw/koj/jetstreem/core/StrmIOLoop.java
@@ -9,6 +9,7 @@ import java.util.*;
 public class StrmIOLoop extends Thread {
     private Selector selector;
     private StrmQueue taskQueue;
+    private int ioWaitNum;
 
     private StrmIOLoop () {
         super();
@@ -18,16 +19,47 @@ public class StrmIOLoop extends Thread {
             System.err.println("I/O error: " + ex.getMessage());
         }
         this.taskQueue = StrmQueue.getInstance();
+        this.ioWaitNum = 0;
     }
 
     public static StrmIOLoop initIOLoop() {
         StrmIOLoop loop = new StrmIOLoop();
-        loop.start();
         return loop;
     }
 
     public void closeIOLoop() {
         this.interrupt();
+    }
+
+    public Selector selector() {
+        return this.selector;
+    }
+
+    public StrmQueue taskQueue() {
+        return this.taskQueue;
+    }
+
+    public int ioWaitNum() {
+        return this.ioWaitNum;
+    }
+
+    public void ioPush(SelectableChannel ch, Streem strm, StrmFunc cb, int key) 
+        throws ClosedChannelException {
+        StrmIOTask task = new StrmIOTask(strm, cb);
+        ch.register(this.selector, key, task);
+    }
+
+    public void ioStart(Streem strm, SelectableChannel ch, StrmFunc cb, int key) {
+        try {
+            ioPush(ch, strm, cb, key);
+            this.ioWaitNum++;
+        } catch (ClosedChannelException ex) {
+            System.err.println("Channel error: " + ex.getMessage());
+        }
+    }
+
+    public void ioStartRead(Streem strm, SelectableChannel ch, StrmFunc cb) {
+        ioStart(strm, ch, cb, SelectionKey.IO_READ);
     }
 
     public void run() {
