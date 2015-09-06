0a620ae6a652f53113421a1a82edf5c1d4696533
Koji Matsumoto <otomustam.ijok@gmail.com>
2015-09-06 19:58:02 +0900

    add getChar(), subCharSequence() to ChannelReadBuffer

--- pw/koj/jetstreem/core/ChannelReadBuffer.java
+++ pw/koj/jetstreem/core/ChannelReadBuffer.java
@@ -1,6 +1,7 @@
 package pw.koj.jetstreem.core;
 
 import java.nio.ByteBuffer;
+import java.nio.CharBuffer;
 import java.nio.channels.Channel;
 import java.nio.channels.ReadableByteChannel;
 import java.io.IOException;
@@ -14,5 +15,32 @@ public class ChannelReadBuffer extends ChannelBuffer {
         ReadableByteChannel c = (ReadableByteChannel)this.ch;
         return c.read(this.buf);
     }
+
+    public void getChar(char[] ch) {
+        this.buf.asCharBuffer().get(ch);
+    }
+
+    public char[] subCharSequence(char ch) {
+        int start;
+        int end;
+        CharBuffer cbuf = this.buf.asCharBuffer();
+
+        start = cbuf.position();
+        end = cbuf.limit();
+        int n;
+        for (n = start; n <= end; n++) {
+            if (cbuf.get(n) == ch) break;
+        }
+
+        if (n > end) {
+            return null;
+        }
+        else {
+            char[] c = new char[n - start + 1];
+            cbuf.get(c);
+            return c;
+        }
+    }
+
 }
 
