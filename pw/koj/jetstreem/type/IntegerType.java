2e5893b61d419141f869c39df08b38569a1fc662
Koji Matsumoto <otomustam.ijok@gmail.com>
2015-09-26 15:01:08 +0900

    add class IntegerType

---
+++ pw/koj/jetstreem/type/IntegerType.java
@@ -0,0 +1,10 @@
+package pw.koj.jetstreem.type;
+
+public class IntegerType extends Type {
+    protected String name;
+
+    public IntegerType(String name) {
+        this.name = name;
+    }
+}
+
