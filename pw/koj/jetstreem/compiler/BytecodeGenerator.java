2d6aa43948a600f16123a9d199e72483e4750b2d
Koji Matsumoto <otomustam.ijok@gmail.com>
2017-05-11 22:50:35 +0900

    Bytecode Generator: added for Namespace, next: Let

--- pw/koj/jetstreem/compiler/BytecodeGenerator.java
+++ pw/koj/jetstreem/compiler/BytecodeGenerator.java
@@ -12,6 +12,57 @@ public class BytecodeGenerator {
     private static final String JOBJ = "java/lang/Object";
     private static final String TOBJ = "Ljava/lang/Object;";
 
+    private String simpleName;
+    private ClassWriter cw;
+    private BytecodeGenerator topLevel;
+    private BytecodeGenerator outer;
+    private List<BytecodeGenerator> innerBgs;
+
+    public BytecodeGenerator(String simpleName, ClassWriter cw, BytecodeGenerator outer) {
+        this.simpleName = simpleName;
+        this.cw = cw;
+        this.topLevel = outer.topLevel();
+        this.outer = outer;
+    }
+
+    public String className() {
+        if (outer == null) {
+            return simpleName;
+        }
+        else {
+            return outerName() + "$" + simpleName;
+        }
+    }
+
+    public String outerName() {
+        if (outer == null) {
+            return "";
+        }
+        else {
+            return outer.className();
+        }
+    }
+
+    public String innerName() {
+        return simpleName;
+    }
+
+    public BytecodeGenerator topLevel() {
+        return topLevel;
+    }
+
+    public BytecodeGenerator outer() {
+        return outer;
+    }
+
+    public ClassWriter cw() {
+        return cw;
+    }
+
+    public List<BytecodeGenerator> innerBgs() {
+        return innerBgs;
+    }
+/*
     private String fullNameOf(Namespace ns) {
         return buildNameFrom(ns.getParent()) + ns.getName();
     }
@@ -24,37 +75,57 @@ public class BytecodeGenerator {
             return fullNameOf(ns.getParent()) + ns.getName() + "$";
         }
     }
+*/
+    public void generate(Namespace ns, Deque<RefTable> ctx) {
+        this.simpleName = ns.getName();
+        this.topLevel = this;
+        this.innerBgs = new ArrayList<>();
 
-    public byte[] generate(Namespace ir, String className) {
-        this.className = className;
 
-        this.cw = new ClassWriter(COMPUTE_FRAMES | COMPUTE_MAXS);
-        ir.accept(this);
-        this.cw.visitEnd();
+        cw = new ClassWriter(COMPUTE_FRAMES | COMPUTE_MAXS);
+        cw.visit(V1_8, ACC_PUBLIC + ACC_SUPER, ns.getName(), null, JOBJ, null);
 
-        return this.cw.toByteArray();
+        ctx.push(ns.getRefTable());
+
+        for (Object stmt : ns.getStmts()) {
+            stmt.accept(this);
+        }
+
+        ctx.pop();
+
+        cw.visitEnd();
+
     }
 
-    public void visit(Namespace ns) {
+    public void visit(Namespace ns, Deque<RefTable> ctx) {
         BytecodeGenerator inner = new BytecodeGenerator(
             ns.getName(),
-            new ClassWriter(COMPUTE_FRAMES | COMPUTE_MAXS));
+            new ClassWriter(COMPUTE_FRAMES | COMPUTE_MAXS),
+            this);
+        topLevel.innerBgs().add(inner);
 
-        if (ns.getParent() == null) {
-            inner.cw().visit(V1_8, ACC_PUBLIC + ACC_SUPER, ns.getName(), null, JOBJ, null);
+
+        ClassWriter incw = inner.cw();
+        incw.visit(v1_8, ACC_SUPER, inner.className(), null, JOBJ, null);
+
+        for (BytecodeGenerator bg = inner; bg != topLevel; bg = bg.outer()) {
+            incw.visitInnerClass(bg.className(), bg.outerName(), bg.innerName(), ACC_STATIC);
+            bg.outer().cw().visitInnerClass(inner.className(), inner.outerName(), inner.innerName(), ACC_STATIC);
         }
-        else {
-            String name = fullNameOf(ns);
-            inner.cw().visit(v1_8, ACC_SUPER, name, null, JOBJ, null);
-            //generate all inner classes by cw.visitInnerClass() 
-            //add generation of this inner class to all parent classes
-        }
+            
+        ctx.push(ns.getRefTable());
 
         for (Object stmt : ns.getStmts()) {
             stmt.accept(inner);
         }
 
-        inner.cw().visitEnd();
+        ctx.pop();
+
+        incw.visitEnd();
+    }
+
+    public void visit(Let let, Deque<RefTable> ctx) {
+        //from here
     }
 }
 
